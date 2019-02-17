34649212ea274abe1e7071fbf7c0ad5c8de8eb92
Andre Wagner <andre.nho@gmail.com>
2019-02-17 18:20:57 -0300

    .

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "env"        {}
+variable "domain"     {}
+variable "subdomain"  {}
+variable "basename"   {}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+provider "aws" {
+    access_key      = "${var.access_key}"
+    secret_key      = "${var.secret_key}"
+    region          = "${var.region}"
+}
+
+# vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
